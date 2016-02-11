ad9df27063ea22a7fafa72c0874e68b399bb5177
Jason Dempsey <jason.dempsey@digipen.edu>
2016-02-11 13:40:54 -0800

    More stuff! Mostly pipeline work!

---
+++ src/cpu/branch_predict.vhdl
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity Branch_Predict is
+  port (
+
+);
+end Branch_Predict;
+
+architecture Sim_Branch_Predict of Branch_Predict is
+
+begin
+
+
+end Sim_Branch_Predict;
--- src/cpu/cpu-proto.vhdl
+++ src/cpu/cpu-proto.vhdl
@@ -12,40 +12,48 @@ use work.declares.all;
 -- Stalling comes from: Memory interface and Decode for load/store
 entity CPU is
   port(
-    clock     : in  std_logic;
+    clock         : in  std_logic;
     -- Memory Interface
-    mem_addr  : out unsigned(word-1 downto 0);
-    mem_data  : in  unsigned(word-1 downto 0);
-    mem_stall : in  std_logic;
-
+    mem_addr      : out word_unsigned;
+    mem_data      : in  word_unsigned;
+    mem_stall     : in  std_logic
+    -- Branch Prediction Interface
+    pbranch_addr  : out word_unsigned;
+    pbranch_value : in  word_unsigned
     );
 end CPU;
 
 architecture Sim_CPU of CPU is
-  signal pc     : unsigned(word-1 downto 0) := (others => "0");
-  signal pc_val : unsigned(word-1 downto 0);
+  signal pc     : word_unsigned := (others => "0");
+  signal pc_val : word_unsigned;
 
   -- Branch Signals
-  signal pred_branch   : unsigned(word-1 downto 0);
-  signal branch_target : unsigned(word-1 downto 0);
+  signal branch_target : word_unsigned;
 
-  signal branch_mux   : unsigned(word-1 downto 0);
+  signal branch_mux   : word_unsigned;
   signal branch_cntrl : std_logic;
 
-  signal mem_addr : unsigned(word-1 downto 0);
-  signal mem_data : unsigned(word-1 downto 0);
+  signal mem_addr : word_unsigned;
+  signal mem_data : word_unsigned;
 
+  signal pc_incr : word_unsigned;
+
+  -- Registers
+  signal reg_addr1 : word_unsigned;
+  signal reg_addr2 : word_unsigned;
+
+  signal exec_instr : word_unsigned;
 begin
 
   -- FETCH
   branch_cntrl <= or_reduce(pred_branch);
 
   with branch_ctrl select branch_mux <=
-    pred_branch   when '0',
+    pbranch_value when '0',
     branch_target when '1';
 
   with or_reduce(branch_mux) select pc_val <=
-    pc + 4     when '0',
+    pc_incr    when '0',
     branch_mux when '1';
 
   -- This will need to multiplex, eventually, as we need to access memory at
@@ -57,9 +65,12 @@ begin
     if rising_edge(clock) then
       pc <= pc_val;
     end if;
+
+    pc_incr <= pc + 4;
   end process;
 
   -- DECODE
+  -- Perform stall and prime the register file.
   process(mem_data)
   begin
     -- TODO: Handle Stall
@@ -78,6 +89,14 @@ begin
 
       -- Arith Regs
       when "0110011" =>
+
+    end process;
+
+    -- EXECUTE
+    -- Perform operation based on opcode, also determine actual branch target.
+    process(exec_instr, reg_data1, reg_data2)
+
+    begin
 
     end process;
 
